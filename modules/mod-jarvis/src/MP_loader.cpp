2b69b274e8c7d86958397dde7f9676abc4e4cea0
Nuno Levezinho <nuno.levezinho@live.com.pt>
2022-12-12 00:39:35 +0000

    Added .gitignore exception  to Jarvis module

---
+++ modules/mod-jarvis/src/MP_loader.cpp
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2016+ AzerothCore <www.azerothcore.org>, released under GNU AGPL v3 license: https://github.com/azerothcore/azerothcore-wotlk/blob/master/LICENSE-AGPL3
+ */
+
+// From SC
+void AddMyPlayerScripts();
+void AddSC_Jarvis_Services();
+
+// Add all
+// cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
+// additionally replace all '-' in the module folder name with '_' here
+void Addjarvis_moduleScripts()
+{
+    AddSC_Jarvis_Services();
+}
+
